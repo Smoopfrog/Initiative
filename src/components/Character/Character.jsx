0b71e10b8caea1c719a88362075fd9c78e241e1e
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-28 01:34:13 -0700

    add death image

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -17,7 +17,7 @@ const Character = props => {
         
       />
       <Conditions />
-      <Button onClick={(event) => props.statChange(event, 'hp', 0)} id={props.id} name={"Death"}/>
+      <Button onClick={(event) => props.statChange(event, 'hp', 0)} type={'death'} id={props.id}/>
       <Button onClick={props.removeChar} id={props.id} type={'trash'} name={<i className="fa-solid fa-trash-can"></i>}/>
     </article>
   )

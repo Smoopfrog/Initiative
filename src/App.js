da1d67cf4cb920fad6daa2a949e20e11f408ad0b
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-09-21 23:00:33 -0700

    initialize home page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './styles/App.scss'
 import Character from './components/Character/Character';
 import Tracker from './components/Tracker';
 import Navbar from './components/Navbar';
-import { Grid } from '@mui/material';
+import background from './images/dnd3.jpg';
+import chest from './images/treasure.png'
 import useApplicationData from './hooks/useApplicationData';
+import { Box, Container, height, Stack, TextField, Typography, FormControlLabel } from '@mui/material';
+import { CheckBox } from '@mui/icons-material';
 
 
 
@@ -20,39 +23,63 @@ function App() {
         ac={character.ac}
         initiative={character.initiative}
         selected={character.selected}
-        statChange = {statChange}
+        statChange={statChange}
         removeChar={removeChar}
       />
     )
   })
 
   return (
-    <div className="App">
-      {/* <Navbar/>
-      <Tracker 
-        addChar={addChar} 
-        nextChar={nextChar} 
-        prevChar={prevChar} 
-        charArray={state}
+    <Box sx={{ display: 'flex', justifyContent: 'row', height: '100vh' }} >
+      <Box
+        item
+
+
+        sx={{
+          height: '100vh',
+          width: { xs: '0vw', md: '60vw' },
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
       />
-      <section className='turn-order'>
-        {charArray}
-      </section> */}
-      <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
+      <Box
+        sx={{
+          height: '100vh',
+          width: { xs: '100vw', md: '40vw' },
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          margin: '20px'
+        }}
+      >
+        <img src={chest} width="50" height="50" />
+        <Typography>
+          Sign in
+        </Typography>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
         />
-    </div>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+        />
+        <FormControlLabel control={<CheckBox defaultChecked />} label="Remember me" />
+      </Box>
+    </Box>
   );
 }
 

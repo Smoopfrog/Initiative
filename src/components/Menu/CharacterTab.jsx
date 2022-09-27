87599ca94394b923485a74d3163a9bf175264200
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-09-27 16:06:04 -0700

    add drop down sort menu

--- src/components/Menu/CharacterTab.jsx
+++ src/components/Menu/CharacterTab.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import Character from "../Character/Character";
+import React, { useState } from "react";
 import PlayerCharacter from "../PlayerCharacter";
 import useApplicationData from "../../hooks/useApplicationData";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Button, Menu, MenuItem, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, TextField } from "@mui/material";
 
 const CharacterTab = () => {
   const { state } = useApplicationData()
+  const [anchorEl, setAnchorEl] = useState(null);
+  // const [sortOrder, setSortOrder] = useState('level');
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const charArray = state.map(character => {
     console.log(character)
@@ -25,8 +35,36 @@ const CharacterTab = () => {
       />
     )
   })
+
   return (
     <Box>
+      <Box>
+        <Button
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          Sort by
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={handleClose}>Name</MenuItem>
+          <MenuItem onClick={handleClose}>Level</MenuItem>
+          <MenuItem onClick={handleClose}>Class</MenuItem>
+          <MenuItem onClick={handleClose}>Race</MenuItem>
+
+        </Menu>
+      </Box>
+      
       <Stack>
         {charArray}
       </Stack>

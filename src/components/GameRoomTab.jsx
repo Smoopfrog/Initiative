1bad0a6e11f382815cd82530274d31c8ca145cb0
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-10-21 13:53:50 -0700

    add button

---
+++ src/components/GameRoomTab.jsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/system";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import InGameCharacter from "./InGameCharacter";
+import Tracker from "./Tracker";
+// import io from "socket.io-client";
+// const socket = io.connect("http://localhost:3001");
+
+const GameRoom = ({setGameCharacters, gameCharacters}) => {
+  console.log('gameCharacters in game room', gameCharacters)
+
+  console.log(gameCharacters)
+  const inGameChars = gameCharacters.map(character => {
+    return (
+      <InGameCharacter character={character} gameCharacters={gameCharacters} setGameCharacters={setGameCharacters}/>
+    )
+  })
+  return (
+    <Box> 
+      <Tracker gameCharacters={gameCharacters} />
+      {inGameChars}
+    </Box>
+  )
+}
+
+export default GameRoom;

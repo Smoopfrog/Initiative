8c40d86c24936cdd4f5afad2beeaff4b57afddad
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-10-21 20:16:56 -0700

    add more info to inGameCharacter component

--- src/components/InGameCharacter.jsx
+++ src/components/InGameCharacter.jsx
@@ -1,42 +1,62 @@
-import { Box, Card, Typography, Input, Button } from "@mui/material";
-import React from "react";
+import { Box, Card, Typography, Input, Button, Link } from "@mui/material";
+import React, { useState } from "react";
+import halfling from "../images/Ivan_Kaslov-0.webp"
 
-const inGameCharacter = ({ character, gameCharacters, setGameCharacters }) => {
+const InGameCharacter = ({ character, gameCharacters, setGameCharacters }) => {
+  const [initiative, setInitiative] = useState(character.initiative);
+  const [hp, setHp] = useState(character.hp);
+  const [ac, setAc] = useState(character.ac);
+
 
   const removeChar = () => {
     let newChars = gameCharacters.filter(newCharacters => newCharacters.id != character.id)
     setGameCharacters(newChars)
   }
 
-  return (
-    <Card sx={{ display: 'flex', padding: '10px', margin: '10px' }}>
-      <Box>
-        <img />
-      </Box>
-      <Box>
-        <Box>
-          <Typography variant='h3'>
-            {character.charName}
-          </Typography>
-          <Typography variant='subtitle'>
-            Lv. {character.level} {character.race} {character.class}
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex' }}>
-          <label>Initiative</label>
-          <Input sx={{ width: '30px' }} value={character.initiative}>
-          </Input>
+  const changeInitiative = (event) => {
+    setInitiative(event.target.value)
+    character.initiative = event.target.value;
+    console.log(character)
+    let newChars = gameCharacters.filter(gameCharacter => gameCharacter.id != character.id)
+    setGameCharacters([...newChars, character])
+  }
 
-          <label>HP</label>
-          <Input sx={{ width: '30px' }} value={character.hp}>
-          </Input>
-          
-          <label>AC</label>
-          <Input sx={{ width: '30px' }} value={character.ac}>
-          </Input>
+  const changeHp = event => {
+    setHp(event.target.value)
+  }
+
+  const changeAc = event => {
+    setAc(event.target.value)
+  }
+
+  return (
+    <Card sx={{ display: 'flex', justifyContent: 'space-between', padding: '10px', margin: '10px' }}>
+      <Box sx={{display: 'flex'}}>
+        <Box sx={{ display: 'flex' }}>
+          <img src={halfling} width='25%' />
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <Typography variant="h5" >{character.charName}</Typography>
+            <Typography variant="h12">Lv.{character.level} {character.race} {character.class}</Typography>
+            {character.charSheetUrl && <Link href={character.charSheetUrl} target="_blank" rel="noopener noreferrer">Character Sheet</Link>}
+          </Box>
+
         </Box>
+        <Box sx={{ display: 'flex', width: '40%' }}>
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', border: '10px black'}}>
+              <label>Initiative</label>
+              <Input value={initiative} onChange={changeInitiative} sx={{ width: '30px', textAlignLast: 'center'  }} />
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlignLast: 'center'}}>
+              <label>HP</label>
+              <Input value={hp} onChange={changeHp} sx={{ width: '30px' }} />
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlignLast: 'center'}}>
+              <label>AC</label>
+              <Input value={ac} onChange={changeAc} sx={{ width: '30px' }} />
+            </Box>
+          </Box>
       </Box>
-      <Box sx={{display:'flex', flexDirection:'column'}}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <Button onClick={removeChar} variant="contained" color="error" type={'trash'}><i className="fa-solid fa-trash-can"></i></Button>
         <Button>?</Button>
       </Box>
@@ -44,4 +64,4 @@ const inGameCharacter = ({ character, gameCharacters, setGameCharacters }) => {
   )
 };
 
-export default inGameCharacter;
+export default InGameCharacter;

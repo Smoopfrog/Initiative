9684e0b4b33c8386c3769d3b8be6c68770434ff6
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-10-24 19:58:42 -0700

    create monsterCard component

--- src/components/Menu/MonsterManualTab.jsx
+++ src/components/Menu/MonsterManualTab.jsx
@@ -1,10 +1,11 @@
 import { Box, Button, TextField } from "@mui/material";
 import React from "react";
 import { useState } from "react";
+import MonsterCard from "../MonsterCard";
 
 const MonsterManualTab = () => {
   const [searchText, setSearchText] = useState('')
-  const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState()
 
   const handleSearchTextChange = event => {
     setSearchText(event.target.value)
@@ -15,13 +16,15 @@ const MonsterManualTab = () => {
     const response = await fetch(`https://www.dnd5eapi.co/api/monsters/${monster}`)
     const data = await response.json();
     setSearchResults(data)
-    console.log(data)
   };
 
   return (
     <Box>
       <TextField id="outlined-basic" label="Search" variant="outlined" value={searchText} onChange={handleSearchTextChange} />
       <Button variant="contained" color="success" onClick={searchMonsterManual}>Search</Button>
+      {searchResults && 
+        <MonsterCard monster={searchResults}/>
+      }
     </Box>
   )
 };
---
+++ src/components/MonsterCard.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+import React from "react";
+
+const MonsterCard = () => {
+  return (
+    <Box>
+      Hi
+    </Box>
+  )
+};
+
+export default MonsterCard;

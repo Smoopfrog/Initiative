83acf23141c0594c9ce2dc179b055c20b14bdeed
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-08 23:17:48 -0800

    styling changes to tracker

--- src/components/GameRoomTab.jsx
+++ src/components/GameRoomTab.jsx
@@ -2,7 +2,7 @@ import { Box, Button } from "@mui/material";
 import InGameCharacter from "./InGameCharacter";
 
 const GameRoom = ({ setGameCharacters, gameCharacters, sortByInitiative }) => {
-  console.log(gameCharacters)
+  console.log(gameCharacters);
   const inGameChars = gameCharacters.map((character) => {
     return (
       <InGameCharacter
@@ -92,26 +92,28 @@ const GameRoom = ({ setGameCharacters, gameCharacters, sortByInitiative }) => {
   };
 
   const playerTurn = () => {
-    const selectedIndex = gameCharacters.findIndex(char => char.selected)
-    console.log(selectedIndex)
-    if (selectedIndex !== -1){
-      return <h1>Turn: {gameCharacters[selectedIndex].charName}</h1>
+    const selectedIndex = gameCharacters.findIndex((char) => char.selected);
+    console.log(selectedIndex);
+    if (selectedIndex !== -1) {
+      return <h1>Turn: {gameCharacters[selectedIndex].charName}</h1>;
     }
-    return <h1>Prepare for battle!</h1>
-  }
+    return <h1>Prepare for battle!</h1>;
+  };
 
   return (
     <Box>
-      <div className="tracker">
+      <Box sx={{ display: "flex", flexDirection: 'column', alignItems: 'center' }}>
         {playerTurn()}
-        <Button onClick={prevChar}>
-          <i className="fa-solid fa-arrow-left"></i>
-        </Button>
-        <Button onClick={sortButtonHandler}>Sort</Button>
-        <Button onClick={nextChar}>
-          <i className="fa-solid fa-arrow-right"></i>
-        </Button>
-      </div>
+        <Box>
+          <Button onClick={prevChar}>
+            <i className="fa-solid fa-arrow-left"></i>
+          </Button>
+          <Button onClick={sortButtonHandler}>Sort</Button>
+          <Button onClick={nextChar}>
+            <i className="fa-solid fa-arrow-right"></i>
+          </Button>
+        </Box>
+      </Box>
       {inGameChars}
     </Box>
   );

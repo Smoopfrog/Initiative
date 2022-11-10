19581c9f676cc4a4e6659ea57885ef219ea1df54
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-09 23:38:54 -0800

    fix search bar if result not found

--- src/components/Menu/MonsterManualTab.jsx
+++ src/components/Menu/MonsterManualTab.jsx
@@ -4,31 +4,49 @@ import { useState } from "react";
 import MonsterCard from "../MonsterCard";
 
 const MonsterManualTab = ({ setGameCharacters }) => {
-  const [searchText, setSearchText] = useState('')
-  const [searchResults, setSearchResults] = useState()
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState();
 
-  const handleSearchTextChange = event => {
-    setSearchText(event.target.value)
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
   };
 
   const searchMonsterManual = async () => {
-    const monster = searchText.trim().replace(/\s+/g, '-').toLowerCase();
-    const response = await fetch(`https://www.dnd5eapi.co/api/monsters/${monster}`)
+    const monster = searchText.trim().replace(/\s+/g, "-").toLowerCase();
+    const response = await fetch(
+      `https://www.dnd5eapi.co/api/monsters/${monster}`
+    );
     const data = await response.json();
-    setSearchResults(data)
+
+    if (!data.name) {
+      setSearchResults(false);
+    } else {
+      setSearchResults(data);
+    }
   };
 
   return (
     <Box>
-      <TextField id="outlined-basic" label="Search" variant="outlined" value={searchText} onChange={handleSearchTextChange} />
-      <Button variant="contained" color="success" onClick={searchMonsterManual}>Search</Button>
-      {searchResults && 
-        <MonsterCard monster={searchResults} setGameCharacters={setGameCharacters}/>
-      }
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        variant="outlined"
+        value={searchText}
+        onChange={handleSearchTextChange}
+      />
+      <Button variant="contained" color="success" onClick={searchMonsterManual}>
+        Search
+      </Button>
+      {searchResults ? (
+        <MonsterCard
+          monster={searchResults}
+          setGameCharacters={setGameCharacters}
+        />
+      ) : (
+        <h2>Monster not found</h2>
+      )}
     </Box>
-  )
+  );
 };
 
 export default MonsterManualTab;
-
-

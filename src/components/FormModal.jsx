5c918acb2bd66531072ed9077c02e18493a9fc53
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-17 22:45:52 -0800

    reformat form

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -54,55 +54,69 @@ const NewCharacterForm = (props) => {
             selectedImage={selectedImage}
             setSelectedImage={setSelectedImage}
           />
-          <label htmlFor="name">Name</label>
-          <input
-            id="name"
-            type="text"
-            value={newCharName}
-            onChange={(e) => setNewCharName(e.target.value)}
-          />
-          <label htmlFor="level">Level</label>
-          <input
-            id="level"
-            type="number"
-            value={newCharLevel}
-            onChange={(e) => setNewCharLevel(e.target.value)}
-          />
-          <label htmlFor="race">Race</label>
-          <input
-            id="race"
-            type="text"
-            value={newCharRace}
-            onChange={(e) => setNewCharRace(e.target.value)}
-          />
-          <label htmlFor="class">Class</label>
-          <input
-            id="class"
-            type="text"
-            value={newCharClass}
-            onChange={(e) => setNewCharClass(e.target.value)}
-          />
-          <label htmlFor="hp">HP</label>
-          <input
-            id="hp"
-            type="number"
-            value={newCharHp}
-            onChange={(e) => setNewCharHp(e.target.value)}
-          />
-          <label htmlFor="ac">AC</label>
-          <input
-            id="ac"
-            type="number"
-            value={newCharAc}
-            onChange={(e) => setNewCharAc(e.target.value)}
-          />
-          <label htmlFor="characterSheet">Character Sheet Link</label>
-          <input
-            id="characterSheet"
-            type="url"
-            value={newCharSheet}
-            onChange={(e) => setNewCharSheet(e.target.value)}
-          />
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              value={newCharName}
+              onChange={(e) => setNewCharName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="level">Level</label>
+            <input
+              id="level"
+              type="number"
+              value={newCharLevel}
+              onChange={(e) => setNewCharLevel(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="race">Race</label>
+            <input
+              id="race"
+              type="text"
+              value={newCharRace}
+              onChange={(e) => setNewCharRace(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="class">Class</label>
+            <input
+              id="class"
+              type="text"
+              value={newCharClass}
+              onChange={(e) => setNewCharClass(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="hp">HP</label>
+            <input
+              id="hp"
+              type="number"
+              value={newCharHp}
+              onChange={(e) => setNewCharHp(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="ac">AC</label>
+            <input
+              id="ac"
+              type="number"
+              value={newCharAc}
+              onChange={(e) => setNewCharAc(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="characterSheet">Character Sheet Link</label>
+            <input
+              id="characterSheet"
+              type="url"
+              value={newCharSheet}
+              onChange={(e) => setNewCharSheet(e.target.value)}
+            />
+          </div>
         </body>
         <footer>
           <Button onClick={submitNewChar}>Create</Button>

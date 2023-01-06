cd283ef2172e91b5db62f68361f373798a0151a1
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-01-05 17:48:24 -0800

    finish delete modal styling

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -1,24 +1,37 @@
 import ModalBackdrop from "./ModalBackdrop";
 import ReactDOM from "react-dom";
 import modalStyles from "./Backdrop.module.css";
-import styles from "./ConfirmationModal.module.css"
+import styles from "./ConfirmationModal.module.css";
 
 const ConfirmationPopUp = (props) => {
   return (
     <div className={modalStyles.modal}>
+      <div onClick={props.closeForm} className={styles.closeIcon}>
+        <i  class="fa-solid fa-x"></i>
+      </div>
       <div className={styles.container}>
-        <span className={styles.icon}>
-          <i class="fa-regular fa-circle-xmark fa-x5"></i>
+        <span className={styles.garbageIcon}>
+          <i className="fa-solid fa-trash-can"></i>
         </span>
         <h1>Are you sure?</h1>
         <p>
           Are you sure you want to delete {props.name}? This process cannot be
           undone.
         </p>
-        <footer>
-          <button className={styles.button + ' ' + styles.cancelButton} onClick={props.closeForm}>Cancel</button>
-          <button className={styles.button + ' ' + styles.deleteButton} onClick={props.onConfirm}>Delete</button>
-        </footer>
+        <div className={styles.buttons}>
+          <button
+            className={styles.button + " " + styles.cancelButton}
+            onClick={props.closeForm}
+          >
+            Cancel
+          </button>
+          <button
+            className={styles.button + " " + styles.deleteButton}
+            onClick={props.onConfirm}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );

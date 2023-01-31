d7096375d91ac959c8c0246aae42bff94efb0676
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-01-30 18:47:59 -0800

    change styling for tabs

--- src/components/Layout/Tabs.jsx
+++ src/components/Layout/Tabs.jsx
@@ -2,13 +2,19 @@ import styles from "./Tabs.module.css";
 import Button from "../UI/Button";
 
 const Tabs = (props) => {
+  console.log(props.currentTab)
+
   return (
     <div className={styles.tabs}>
-      <Button onClick={() => props.onClick("characters")}>Characters</Button>
-      <Button onClick={() => props.onClick("monsterManual")}>
+      <Button onClick={() => props.onClick("characters")} style={props.currentTab === "characters" ? "tab-active" : "tab"}>
+        Characters
+      </Button>
+      <Button onClick={() => props.onClick("monsterManual")} style={props.currentTab === "monsterManual" ? "tab-active" : "tab"}>
         Monster Manual
       </Button>
-      <Button onClick={() => props.onClick("profile")}>Profile</Button>
+      <Button onClick={() => props.onClick("profile")} style={props.currentTab === "profile" ? "tab-active" : "tab"}>
+        Profile
+      </Button>
     </div>
   );
 };

00277c74072fe8dfa1ab0fb85675b25cd9a9223b
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-03-04 23:42:59 -0800

    swap level for cr and import decimal to fraction converter

--- src/components/Layout/SideBar/MonsterManual/MonsterCard.jsx
+++ src/components/Layout/SideBar/MonsterManual/MonsterCard.jsx
@@ -6,10 +6,16 @@ import {
   selectGameRoomCharacters,
   setGameRoomCharacters,
 } from "../../../../slices/gameRoomSlice";
-
+import { fraction } from "mathjs";
 const MonsterCard = ({ monster }) => {
   const dispatch = useDispatch();
   const gameRoomCharacters = useSelector(selectGameRoomCharacters);
+
+  let cr = monster["challenge_rating"];
+  if (cr < 1) {
+    const crValues = fraction(cr);
+    cr = `${crValues.n}/${crValues.d}`;
+  }
 
   const rollTwentyLink = `https://roll20.net/compendium/dnd5e/${monster.name}`;
 
@@ -33,14 +39,17 @@ const MonsterCard = ({ monster }) => {
     <div className={styles.card}>
       <div className={styles.character}>
         <div className={styles.info}>
-          <h1 className={styles["char-name"]}>
-            <a href={rollTwentyLink} target="_blank" rel="noreferrer noopener">
-              {monster.name}
-            </a>
-          </h1>
+          <a
+            className={styles["char-name"]}
+            href={rollTwentyLink}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {monster.name}
+          </a>
           <div className={styles["char-details"]}>
             <p className={styles["char-info"]}>
-              Lv.{monster.level} {monster.race} {monster.class}
+              CR {cr} {monster.race} {monster.class}
               {monster.size} {monster.type}
               {monster.subtype && ` (${monster.subtype})`}
             </p>

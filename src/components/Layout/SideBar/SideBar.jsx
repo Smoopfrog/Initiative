e8548de44c3fbdc8c1325b893ad7dd10c1c17f39
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-10 14:15:57 -0700

    change sidebar layout for mobile

--- src/components/Layout/SideBar/SideBar.jsx
+++ src/components/Layout/SideBar/SideBar.jsx
@@ -31,20 +31,7 @@ const SideBar = ({ setHomepage }) => {
         <div className={styles.title}>Initiative</div>
         <div className={styles["btn-container"]}>
           <Button style="toggle-sidebar" onClick={toggleSideBar}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="chevron"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
-              />
-            </svg>
+            <i class="fa-solid fa-bars fa-xl"></i>
           </Button>
         </div>
       </div>
